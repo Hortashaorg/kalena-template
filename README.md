fe32a9f960944f477a4dbc0801bc42422ca2ed4e
Hortasha <eidemartin_303@hotmail.com>
2025-02-15 21:19:32 +0000

    lint and format

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Getting Started
 
 ## Tailwind CLI
+
 ```bash
 curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
 chmod +x tailwindcss-linux-x64
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { app } from "@kalena/framework";
 
 const myApp = app({
-    routes: [],
-    port: 4000,
-  });
-  
-  Deno.serve({
-    port: 4000,
-    hostname: "127.0.0.1",
-  }, myApp.fetch);
-  
+  routes: [],
+  port: 4000,
+});
+
+Deno.serve({
+  port: 4000,
+  hostname: "127.0.0.1",
+}, myApp.fetch);
